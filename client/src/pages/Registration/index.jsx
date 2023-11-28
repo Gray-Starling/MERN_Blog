25e1999c412a1b82dbca77668b01365e7d6324bb
Sergey <skvorcovsa90@gmail.com>
2023-11-28 10:28:13 +1000

    auth and post logic

--- client/src/pages/Registration/index.jsx
+++ client/src/pages/Registration/index.jsx
@@ -1,27 +1,85 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Avatar from '@mui/material/Avatar'
 
-import styles from './Login.module.scss';
+import styles from './Login.module.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAuth, selectIsAuth } from '../../store/slices/auth'
+import { useForm } from 'react-hook-form'
+import { Navigate } from 'react-router-dom'
 
 export const Registration = () => {
-  return (
-    <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
-        Создание аккаунта
-      </Typography>
-      <div className={styles.avatar}>
-        <Avatar sx={{ width: 100, height: 100 }} />
-      </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Зарегистрироваться
-      </Button>
-    </Paper>
-  );
-};
+	const isAuth = useSelector(selectIsAuth)
+	const dispatch = useDispatch()
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			fullName: 'Вася Пупкин',
+			email: 'vasy@mail.ru',
+			password: 'qwertyu',
+		},
+		mode: 'onChange',
+	})
+
+	const onSubmit = async values => {
+		const data = await dispatch(fetchAuth(values))
+		if (!data.payload) {
+			return alert('Не удалось авторизоваться')
+		}
+		if ('token' in data.payload) {
+			window.localStorage.setItem('token', data.payload.token)
+		}
+	}
+
+	if (isAuth) {
+		return <Navigate to='/' />
+	}
+
+	return (
+		<Paper classes={{ root: styles.root }}>
+			<Typography classes={{ root: styles.title }} variant='h5'>
+				Создание аккаунта
+			</Typography>
+			<div className={styles.avatar}>
+				<Avatar sx={{ width: 100, height: 100 }} />
+			</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<TextField
+					error={Boolean(errors.fullName?.message)}
+					helperText={errors.fullName?.message}
+					{...register('fullName', { required: 'Укажите имя' })}
+					fullWidth
+					className={styles.field}
+					label='Полное имя'
+				/>
+				<TextField
+					error={Boolean(errors.email?.message)}
+					helperText={errors.email?.message}
+					{...register('email', { required: 'Укажите почту' })}
+					fullWidth
+					className={styles.field}
+					label='E-Mail'
+				/>
+				<TextField
+        type='password'
+					error={Boolean(errors.password?.message)}
+					helperText={errors.password?.message}
+					{...register('password', { required: 'Укажите пароль' })}
+					fullWidth
+					className={styles.field}
+					label='Пароль'
+				/>
+				<Button type='submit' size='large' variant='contained' fullWidth>
+					Зарегистрироваться
+				</Button>
+			</form>
+		</Paper>
+	)
+}
